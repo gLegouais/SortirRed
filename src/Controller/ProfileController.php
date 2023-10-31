46ab3cc69861f320f1d05c7fdb7b1e25d132ad8d
glegouais <glegouaisgit@gmail.com>
2023-10-31 14:05:54 +0100

    Quo Mite

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -12,9 +12,8 @@ class ProfileController extends AbstractController
     #[Route('/profile/{id}', name: 'user_profile', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function userProfile(int $id, UserRepository $userRepo): Response
     {
-        //$user = $userRepo -> find($id);
-        $user = $this -> getUser() -> getUserIdentifier();
-        var_dump($user);
+        $user = $userRepo -> find($id);
+//        $user = $this -> getUser();
         return $this->render('profile/profile.html.twig', [
             'user' => $user
         ]);
