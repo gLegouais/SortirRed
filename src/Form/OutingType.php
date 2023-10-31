191a59e483adb3ea60accb63dca6905e8e39a122
Quentin Scaerou <quentin.scaerou@gmail.com>
2023-10-31 14:46:41 +0100

    Added form to create a new outing

---
+++ src/Form/OutingType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use App\Entity\Outing;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OutingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la sortie'
+            ])
+            ->add('startDate', DateTimeType::class, [
+                'label' => 'Date et heure de la sortie'
+            ])
+            ->add('deadline', DateTimeType::class, [
+                'label' => 'Date limite d\'inscription'
+            ])
+            ->add('maxRegistered', IntegerType::class, [
+                'label' => 'Nombre de places'
+            ])
+            ->add('duration', IntegerType::class, [
+                'label' => 'Durée'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description et infos'
+            ])
+            ->add('city', EntityType::class, [
+                'label' => 'Ville',
+                'class' => City::class,
+                'choice_label' => 'name',
+                'placeholder' => '-- Choisir une ville --'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Outing::class,
+        ]);
+    }
+}
