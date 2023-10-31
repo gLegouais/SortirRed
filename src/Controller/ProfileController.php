1101abc09a87c6ece3ea622758f4cf2eb24169c5
glegouais <glegouaisgit@gmail.com>
2023-10-31 13:46:31 +0100

    Quo Mite

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'user_profile')]
-    public function index(): Response
+    #[Route('/profile/{id}', name: 'user_profile', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function userProfile(int $id, UserRepository $userRepo): Response
     {
+        //$user = $userRepo -> find($id);
+        $user = $this -> getUser() -> getUserIdentifier();
+        var_dump($user);
         return $this->render('profile/profile.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $user
         ]);
     }
 }
