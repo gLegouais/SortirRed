58712fac7d87410df303773e40c2343bde69878e
elise-achard <elise.achard.pro@gmail.com>
2023-10-31 12:23:51 +0100

    home -

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -13,7 +13,7 @@ class LoginController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
          if ($this->getUser()) {
-             return $this->redirectToRoute('main_home');
+             return $this->redirectToRoute('home_list');
          }
 
         // get the login error if there is one
