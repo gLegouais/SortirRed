18f391384a2a390bbf9d99817ad548c3ffb2031d
glegouais <glegouaisgit@gmail.com>
2023-11-02 16:25:44 +0100

    Quo Mite

--- src/Form/Model/SearchOutingFormModel.php
+++ src/Form/Model/SearchOutingFormModel.php
@@ -2,14 +2,102 @@
 
 namespace App\Form\Model;
 
+use App\Entity\Outing;
+
 class SearchOutingFormModel
 {
-    public $campus;
-    public $name;
-    public $startDate;
-    public $endDate;
-    public $outingOrganizer;
-    public $outingEnlisted;
-    public $outingNotEnlisted;
-    public $outingFinished;
+    private $campus;
+    private $name;
+    private $startDate;
+    private $endDate;
+    private $outingOrganizer;
+    private $outingEnlisted;
+    private $outingNotEnlisted;
+    private $outingFinished;
+
+    public function getCampus()
+    {
+        return $this->campus;
+    }
+
+    public function setCampus($campus): void
+    {
+        $this->campus = $campus;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate($startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    public function getEndDate()
+    {
+//        $outing = new Outing();
+//        $starDate = $outing->getStartDate();
+//        $duration = $outing->getDuration();
+//
+//        $endDate = $starDate->modify('+' . $duration . 'days');
+        return $this -> endDate;
+    }
+
+    public function setEndDate($endDate): void
+    {
+        $this -> endDate = $endDate;
+    }
+
+    public function getOutingOrganizer()
+    {
+        return $this->outingOrganizer;
+    }
+
+    public function setOutingOrganizer($outingOrganizer): void
+    {
+        $this->outingOrganizer = $outingOrganizer;
+    }
+
+    public function getOutingEnlisted()
+    {
+        return $this->outingEnlisted;
+    }
+
+    public function setOutingEnlisted($outingEnlisted): void
+    {
+        $this->outingEnlisted = $outingEnlisted;
+    }
+
+    public function getOutingNotEnlisted()
+    {
+        return $this->outingNotEnlisted;
+    }
+
+    public function setOutingNotEnlisted($outingNotEnlisted): void
+    {
+        $this->outingNotEnlisted = $outingNotEnlisted;
+    }
+
+    public function getOutingFinished()
+    {
+        return $this->outingFinished;
+    }
+
+    public function setOutingFinished($outingFinished): void
+    {
+        $this->outingFinished = $outingFinished;
+    }
+
 }
