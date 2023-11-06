4119e61ea9cb2b194236dbb5b707a1ddd86540a4
elise-achard <elise.achard.pro@gmail.com>
2023-11-06 12:55:16 +0100

    Formulaire pour annuler une sortie (incomplet)

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Location;
 use App\Entity\Outing;
+use App\Form\CancellationType;
 use App\Form\Model\OutingTypeModel;
 use App\Form\Model\SearchOutingFormModel;
 use App\Form\OutingType;
+use App\Form\ProfileType;
 use App\Form\SearchOutingType;
 use App\Repository\CityRepository;
 use App\Repository\LocationRepository;
@@ -23,15 +25,15 @@ class OutingController extends AbstractController
 {
     #[Route('/', name: 'home_list', methods: ['GET', 'POST'])]
     public function listOuting(
-        OutingRepository $outingRepository,
-        StatusRepository $status,
+        OutingRepository       $outingRepository,
+        StatusRepository       $status,
         EntityManagerInterface $em,
-        Request $request
+        Request                $request
     ): Response
     {
         $searchOutingFormModel = new SearchOutingFormModel();
-        $searchForm = $this -> createForm(SearchOutingType::class, $searchOutingFormModel);
-        $searchForm -> handleRequest($request);
+        $searchForm = $this->createForm(SearchOutingType::class, $searchOutingFormModel);
+        $searchForm->handleRequest($request);
 
         $currentDate = new \DateTimeImmutable();
 
@@ -67,12 +69,12 @@ class OutingController extends AbstractController
 
         // if form submitted
         // $outings = requete sql
-        if($searchForm -> isSubmitted() && $searchForm -> isValid()) {
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
             //dd($searchOutingFormModel);
             // $outings = $outingRepository -> findByCampus($searchOutingFormModel -> getCampus() -> getId());
             $outings = $outingRepository->filterOutings($searchOutingFormModel, $this->getUser());
-            if(!$outings){
-                $this -> addFlash('danger', 'Pas de sortie prévue sur ce campus');
+            if (!$outings) {
+                $this->addFlash('danger', 'Pas de sortie prévue sur ce campus');
             }
         }
 
@@ -189,10 +191,10 @@ class OutingController extends AbstractController
 
     #[Route('/withdrawal/{id}', name: 'outing_withdrawal', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function withdrawal(
-        int $id,
-        OutingRepository $outingRepository,
+        int                    $id,
+        OutingRepository       $outingRepository,
         EntityManagerInterface $em,
-        Request $request
+        Request                $request
     ): Response
     {
         $outing = $outingRepository->find($id);
@@ -211,15 +213,15 @@ class OutingController extends AbstractController
 
     #[Route('/publication/{id}', name: 'outing_publication', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function publication(
-        int $id,
-        OutingRepository $outingRepository,
-        StatusRepository $statusRepository,
+        int                    $id,
+        OutingRepository       $outingRepository,
+        StatusRepository       $statusRepository,
         EntityManagerInterface $em,
-        Request $request
+        Request                $request
     ): Response
     {
         $outing = $outingRepository->find($id);
-        if($outing->getStatus()->getLabel() == 'Created'){
+        if ($outing->getStatus()->getLabel() == 'Created') {
             $outing->addParticipant($outing->getOrganizer());
             $outing->setStatus($statusRepository->findOneBy(['label' => 'Open']));
             $this->addFlash('success', 'Votre proposition de sortie a été publiée !');
@@ -232,6 +234,7 @@ class OutingController extends AbstractController
 
     }
 
+    /* //Ancienne méthode d'annulation
 #[Route('/cancellation/{id}', name: 'outing_cancellation', requirements:['id'=>'\d+'], methods: ['GET'])]
 public function cancellation(
     int $id,
@@ -250,27 +253,69 @@ public function cancellation(
     $em->persist($outing);
     $em->flush();
 
+    // Ancien return, quand il suffisait de cliquer sur "Annuler la sortie" pour le faire. (pas de motif d'annulation)
     $referer = $request->headers->get('referer');
     return $this->redirect($referer);
 }
+    */
+
+    #[Route('/cancellation/{id}', name: 'outing_cancellation', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function cancellation(
+        int                    $id,
+        Outing                 $outing,
+        OutingRepository       $outingRepository,
+        StatusRepository       $statusRepository,
+        EntityManagerInterface $em,
+        Request                $request): Response
+    {
+        var_dump("Je suis dans LA PREMIERE PARTIE DE ma méthode d'annulation de sortie");
+        $outing = $outingRepository->find($id); //besoin de récupérer l'id à ce moment-là ?
+        $cancellationForm = $this->createForm(CancellationType::class, $outing); //est-ce que c'est le bon nom de formulaire ??
+        $cancellationForm->handleRequest($request);
+
+        //peut-être rendre ma condition plus simple. Vérifier le statut est peut-être inutile.
+        if (($cancellationForm->isSubmitted() && $cancellationForm->isValid()) && (($outing->getStatus()->getLabel() == 'Open') || ($outing->getStatus()->getLabel() == 'Closed'))) {
+
+            var_dump("Je suis dans LA DEUXIEME PARTIE DE ma méthode d'annulation de sortie  : soumission et validation du formulaire");
+            //il faudrait une action ici pour que ma nouvelle description soit ajoutée dans la bdd ?
+
+            $outing->setStatus($statusRepository->findOneBy(['label' => 'Cancelled'])); //changement du statut en "Cancelled"
+            $participants = $outing->getParticipants(); //suppression des participants
+            $participants->clear();
+
+            $em->persist($outing);
+            $em->flush();
+            $this->addFlash('success', 'Vous avez supprimé votre proposition de sortie !');
+
+            return $this->redirectToRoute('outing_show', ['id' => $outing->getId()]);
+        }
+
+        return $this->render('outing/cancel.html.twig', [
+            'outing' => $outing,
+            'cancellationForm' => $cancellationForm
+        ]);
+
+    }
+
+
     #[Route('/campus/{id}', name: 'search_campus', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function findByCampus(int $id, OutingRepository $outingRepository): Response
     {
-        $outingCampus = $outingRepository -> findByCampus($id);
-        if(!$outingCampus){
-            throw $this -> createNotFoundException('Pas de sortie prévue sur ce campus');
+        $outingCampus = $outingRepository->findByCampus($id);
+        if (!$outingCampus) {
+            throw $this->createNotFoundException('Pas de sortie prévue sur ce campus');
         }
-        return $this -> render('outing/list.html.twig');
+        return $this->render('outing/list.html.twig');
     }
 
     #[Route('/delete/{id}', name: 'delete_outing', requirements: ['id' => '\d+'], methods: ['GET'])]
     public function deleteOuting(
-        int $id,
-        OutingRepository $outingRepository,
+        int                    $id,
+        OutingRepository       $outingRepository,
         EntityManagerInterface $em): Response
     {
         $outing = $outingRepository->find($id);
-        if ($outing->getStatus()->getLabel() == 'Created' ) {
+        if ($outing->getStatus()->getLabel() == 'Created') {
             $this->addFlash('success', 'Votre projet de sortie a été supprimé. ');
             $em->remove($outing);
         }
