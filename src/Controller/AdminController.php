024403c0325af081459218b1d18b105e724f3ef1
Quentin Scaerou <quentin.scaerou@gmail.com>
2023-11-08 09:29:55 +0100

    Cleaned admin controller and templates

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,15 +11,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[Route('/admin')]
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'admin_dashboard')]
     public function dashboard(): Response
     {
-        return $this->render('admin/dashboard.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/dashboard.html.twig');
     }
 
 
